e24c126f2a737a38ef5d2a52f47bbc289ee3f059
VincentDrevet <vi.drevet@gmail.com>
2020-08-19 21:27:32 +0200

    script compilation dynamique

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-Buildroot_Path=""
+Buildroot_Path="/home/vincent/br-tree/board/rpi-3/ovl/bin"
 
 if [ -z "$1" ]
 then
@@ -16,7 +16,7 @@ fi
 
 if [ $1 = "arm" ]
 then
-    CGO_ENABLED=1 CC=/home/vdrevet/Perso/embedded/br-tree/board/rpi-3/cross/bin/arm-linux-gcc GOOS=linux GOARCH=arm go build -ldflags "-linkmode external -extldflags -static" -a vdshell.go show.go authentification.go shell.go display.go fs.go alimentation.go services.go bash.go
+    CGO_ENABLED=1 CC=/home/vincent/br-tree/board/rpi-3/cross/bin/arm-linux-gcc GOOS=linux GOARCH=arm go build -ldflags "-linkmode external -extldflags -static" -a vdshell.go show.go authentification.go shell.go display.go fs.go alimentation.go services.go bash.go sqlmanage.go
     mv ./vdshell $Buildroot_Path
 fi
 
